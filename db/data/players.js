9677f14926eefdf306a942aec867dbe84a92a993
ArthurVillela <arthur.villela1@gmail.com>
2024-06-14 16:27:11 +0200

    new files

---
+++ db/data/players.js
@@ -0,0 +1,14 @@
+const players =[
+    {
+        "playerCost": 1000000,
+        "name": "Steven Gerrard",
+        "nationality": "England",
+        "position": "Midfielder",
+        "pac": 77,
+        "sho": 90,
+        "pas": 90,
+        "dri": 85,
+        "def": 77,
+        "phy": 84,
+    },
+]
