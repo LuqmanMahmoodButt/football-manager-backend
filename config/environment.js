1714957cb895a7ab793bea3a9907508f4d909cf9
Luqman B <luqmanmbutt.dev@gmail.com>
2024-06-21 11:15:28 +0100

    updated db enviroment

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/Football-Manager';
+export const dbURI = process.env.MONGO_DB_URL || 'mongodb://127.0.0.1/Football-Manager';
 export const port = process.env.PORT || 4000;
 export const secret = process.env.SECRET || 'ManchesterUnitedFlamengo';
 
