0854cef0f19fac2270c69b8d3195f0ea3c89e0e1
Luqman B <luqmanmbutt.dev@gmail.com>
2024-06-21 09:49:30 +0100

    deployment logic added

---
+++ netlify/functions/api.js
@@ -0,0 +1,37 @@
+import { port } from '../../config/environment.js';
+import express from 'express';
+import mongoose from 'mongoose';
+import teamRouter from '../../controllers/teams.js';
+import playerRouter from '../../controllers/players.js';
+import authRouter from '../../controllers/auth.js';
+import errorHandler from '../../middleware/errorHandler.js';
+import logger from './middleware/logger.js';
+import { connectToDb } from './db/helpers.js';
+import 'dotenv/config';
+import cors from 'cors'
+import serverless from 'serverless-hhtp'
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+app.use('/', logger);
+app.use('/api',authRouter)
+app.use('/api',playerRouter)
+app.use('/api',teamRouter)
+app.use(errorHandler);
+
+async function startServer() {
+    try {
+      await connectToDb();
+      console.log('⚽Database connected');
+    } catch (err) {
+      console.log('⚽Oh no something went wrong');
+      console.log(err);
+    }
+  }
+
+  startServer();
+
+  export const handler = serverless(app)
